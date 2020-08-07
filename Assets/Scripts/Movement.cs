fc65780a5590db7433399fe70c1af6d4cf5b9853
GeordieGreen <68436694+GeordieGreen@users.noreply.github.com>
2020-08-07 09:17:47 -0700

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(moveX, 0, moveZ);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    public float speed = 5.0f;
+    public float gravity = 9.81f;
+    public float jumpForce = 30f;
+    public float doubleJump = 0.5f;
+
+    float directionY;
+
+    private bool isDoubleJump = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float yMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 facing = new Vector3(xMovement, 0, yMovement);
+
+        if (controller.isGrounded)
+        {
+            isDoubleJump = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                directionY = jumpForce;
+            }
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isDoubleJump)
+            {
+                directionY = jumpForce * doubleJump;
+                isDoubleJump = false;
+            }
+        }
+
+        
+
+        directionY -= gravity * Time.deltaTime;
+
+        facing.y = directionY;
+
+        controller.Move(facing * speed * Time.deltaTime);
+
+        
+    }
+}
